ddc3c00a1bb25011ee8184ce1315b31921d297bc
huy <havu6544@gmail.com>
2023-08-01 13:36:32 +0700

    create FE home page

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  // ...
+  esbuild: {
+    jsxFactory: 'h', // or any other valid JSX factory
+    jsxFragment: 'Fragment', // or any other valid JSX fragment
+  },
+};
