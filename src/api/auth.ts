d764beae46ccf432e2ccf6e7cd839a8cb503385a
baontph <baontph21353@gmail.com>
2023-08-03 22:59:58 +0700

    signin - signup

---
+++ src/api/auth.ts
@@ -0,0 +1,23 @@
+import axios from "axios"
+import { SigninForm, SignupForm } from "../interface/auth/auth";
+
+export const signup = (data: SignupForm) => {
+  const uri = "/signup";
+  return ins.post(uri, data);
+};
+
+export const signin = (data: SigninForm) => {
+  const uri = "/signin";
+  return ins.post(uri, data);
+};
+
+
+const ins = axios.create({
+  baseURL: "http://localhost:3000",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "Bearer token",
+  },
+});
+
+export default ins;
