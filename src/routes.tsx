c232d00015ba5f1a71ea772c36dc42247e3930dd
manhld <manhthe21082003@gmail.com>
2023-08-03 20:11:50 +0700

    Create-UI-Signup-Signin

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,23 +7,31 @@ import Cart from "./components/cart/Cart";
 import Payment from "./components/payment/Payment";
 import ListProducts from "./components/admin/ListProducts";
 import AdminLayouts from "./components/layouts/AdminLayout";
+import Signup from "./components/signup/Signup";
+import Signin from "./components/signin/Signin";
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <WebsiteLayout />,
-        children: [
-            { index: true, element: <HomePage /> },
-            { path: "product", element: <ProductPage /> },
-            { path: "product/:id", element: <ProductDetailPage /> },
-            { path: "cart", element: <Cart /> },
-            { path: "product/payment", element: <Payment /> }
-        ]
-    },
-    {
-        path: "/admin",
-        element: <AdminLayouts />,
-        children: [
-            { index: true, element: <ListProducts /> },
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <WebsiteLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "product", element: <ProductPage /> },
+      { path: "product/:id", element: <ProductDetailPage /> },
+      { path: "cart", element: <Cart /> },
+      { path: "product/payment", element: <Payment /> },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <AdminLayouts />,
+    children: [{ index: true, element: <ListProducts /> }],
+  },
+  {
+    path: "signup",
+    element: <Signup />,
+  },
+  {
+    path: "signin",
+    element: <Signin />,
+  },
+]);
